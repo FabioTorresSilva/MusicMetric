c0f21cfe7a5316849825fa1195d82e5705ea3384
Gabriela Nunes <gabrielassnunes@gmail.com>
2024-02-08 15:05:57 +0000

    artist

--- src/pages/MusicPage.js
+++ src/pages/MusicPage.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Play } from '../components/icons/Icons';
+
 
 const MusicPage = () => {
   return (
     <div>
-      <h1 className='flex font-semibold text-white text-2xl justify-center'>Play</h1>
-      <div>
-        <Play/>
-      </div>
-      
       
     </div>
-  );
-};
+  )
+}
 
 export default MusicPage;
